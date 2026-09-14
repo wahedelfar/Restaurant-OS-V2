6cf8907717d9f8268f3fed96fe59c2529ad4d9fc
wahedelfar <damietta4@gmail.com>
2026-09-14 22:23:31 +0300

    fix: route kitchen page to KDS endpoint

--- app/kitchen/page.tsx
+++ app/kitchen/page.tsx
@@ -1,3 +1,3 @@
 export default function KitchenPage(){
-  return <iframe src="/kds/index.html" style={{width:'100vw',height:'100vh',border:'none'}} title="kitchen" />
+  return <iframe src="/kds/" style={{width:'100vw',height:'100vh',border:'none'}} title="kitchen" />
 }
