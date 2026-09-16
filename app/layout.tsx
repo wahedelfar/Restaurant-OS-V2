04fb5d37a9fecf7233d408e49e7726628c5d2b20
wahedelfar <damietta4@gmail.com>
2026-09-16 04:51:07 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import './globals.css'
 export default function RootLayout({children}:{children:React.ReactNode}){
- return <html lang="ar"><body style={{background:'white',color:'black',minHeight:'100vh'}}>{children}</body></html>
+ return <html lang="ar" dir="rtl"><body style={{background:'white',color:'black'}}>{children}</body></html>
 }
